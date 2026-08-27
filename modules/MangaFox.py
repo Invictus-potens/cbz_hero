8bf4ccef17806fe322aa0c183aeff712ab00c401
Felipe Prado <handplays2015@gmail.com>
2026-08-26 21:57:03 -0300

    Corrigido(MangaFox): site trocou HTML estático por AJAX ofuscado, módulo reescrito
    - getChapterUrls: regex esperava link protocolo-relativo (//fanfox.net/...), site manda relativo puro; aceita os dois formatos agora
    - getPageUrls/getImageUrl: site abandonou <select>+id="image" e passou a servir páginas via AJAX (chapterfun.ashx) com chave anti-scrape (dm5_key) empacotada com o packer Dean Edwards - mesmo esquema do MangaHere.py (mesmo provedor). Reescrito seguindo o padrão já usado lá (util.unpackJs, resposta pix/pvalue)
    - getImageHeaders: CDN de imagem (zjcdn.mangafox.me) responde 403 sem header Referer; adicionado
    - Validado com download real: 5 obras (One Piece, Yakusoku no Neverland, Naruto, Bleach, Jujutsu Kaisen), 3 capítulos cada, 14/15 sucesso (1 falha transitória de CDN, confirmada retestável). 14 CBZ gerados validados: ordem de página correta, toda imagem com magic bytes JPEG real, sem página de erro disfarçada de imagem

--- modules/MangaFox.py
+++ modules/MangaFox.py
@@ -1,6 +1,8 @@
 import web
 import util
 import re
+import time
+import base64
 import feedback
 import ComicEngine
 
@@ -24,6 +26,10 @@ class ComicSite(web.WebResource):
         newstring = re.sub("https://", "http://", url)
         return newstring
 
+    def getImageHeaders(self):
+        # the image CDN answers 403 to referer-less requests
+        return {'Referer': "http://%s/" % valid_domains[0]}
+
 class Comic(ComicSite):
     
     def __init__(self, url):
@@ -44,10 +50,11 @@ class Comic(ComicSite):
         for item in obj_a:
             if not "href" in item.attrib.keys():
                 continue
-            m = re.match("""(//%s/manga/%s/[^"]+)"""%(self.domain,self.name ), item.attrib["href"])
+            # site serves relative hrefs (/manga/name/...) now; used to be protocol-relative (//domain/manga/name/...)
+            m = re.match(r"""(?:https?:)?(?://%s)?(/manga/%s/[^"]+)"""%(re.escape(self.domain),re.escape(self.name) ), item.attrib["href"])
             if not m:
                 continue
-            target_url = "http:"+m.group(1)
+            target_url = "http://%s%s" % (self.domain, m.group(1))
             if not target_url in urls:
                 urls.append(target_url)
 
@@ -78,37 +85,82 @@ class Chapter(ComicSite):
         i = self.url.rfind('/')
         return self.url[:i]
 
+    def getImageBatch(self, base_url, chapter_id, page, key):
+        """ Ask the chapterfun.ashx AJAX endpoint for the images starting at a page
+
+        Site answers with packed javascript declaring a host prefix (pix) and
+        the paths of that page and the next one (pvalue) - same scheme as
+        MangaHere's chapterfun.ashx. Returns the full URLs.
+        """
+        endpoint = "%s/chapterfun.ashx?cid=%s&page=%i&key=%s" % (base_url, chapter_id, page, key)
+        resource = web.WebResource(endpoint, headers={'Referer': "%s/1.html" % base_url})
+
+        unpacked = util.unpackJs(resource.getSource() )
+
+        if unpacked == None:
+            return []
+
+        prefix = re.search('pix\\s*=\\s*"([^"]*)"', unpacked)
+        values = re.search('pvalue\\s*=\\s*\\[(.*?)\\]', unpacked)
+
+        if prefix == None or values == None:
+            return []
+
+        return [ re.sub("^//", "https://", prefix.group(1) + path) for path in re.findall('"([^"]+)"', values.group(1) ) ]
+
     def getPageUrls(self):
-
-        doc = self.getDomObject()
-        top_bar = doc.get_element_by_id("top_bar")
-        #options = top_bar.get_elements_by_tag_name("option")
-        options = top_bar.cssselect("option")
-        urls = []
         base_url = self.getBaseChapterUrl()
+        source   = self.getSource()
 
-        for option in options:
-            m = re.match("^[0-9]+$", option.attrib["value"])
-            if not m:
-                continue
-            v = int(m.group(0))
-            if v < 1:
-                continue
+        chapter_id  = re.search("chapterid\\s*=\\s*([0-9]+)", source)
+        image_count = re.search("imagecount\\s*=\\s*([0-9]+)", source)
+        # the anti-scrape key (dm5_key) ships packed with Dean Edwards' packer
+        packed_key  = util.unpackJs(source)
 
-            urls.append( "%s/%i.html" % (base_url, v) )
+        if chapter_id == None or image_count == None or packed_key == None:
+            raise ComicEngine.ComicError("Could not read the page list of %s" % self.url)
 
-        util.naturalSort(urls, ".+/([0-9.]+)\\.html")
-        return urls
+        chapter_id  = chapter_id.group(1)
+        image_count = int(image_count.group(1) )
+        key         = ''.join(re.findall("'([0-9a-z])'", packed_key) )
+
+        image_urls = []
+        page = 1
+
+        while len(image_urls) < image_count and page <= image_count:
+            batch = self.getImageBatch(base_url, chapter_id, page, key)
+
+            if len(batch) == 0:
+                raise ComicEngine.ComicError("No images listed for page %i of %s" % (page, self.url) )
+
+            for image_url in batch:
+                if image_url not in image_urls:
+                    image_urls.append(image_url)
+
+            feedback.debug("%i/%i images" % (len(image_urls), image_count) )
+            page += len(batch)
+            time.sleep(recommended_delay)
+
+        # the page number and its image are stuffed in a bogus query string
+        page_urls = []
+
+        for index in range(len(image_urls) ):
+            page_urls.append("%s/%i.html?u=%s" % (
+                base_url,
+                index + 1,
+                base64.urlsafe_b64encode(image_urls[index].encode("utf-8") ).decode("utf-8")
+                ) )
+
+        return page_urls
 
 class Page(ComicSite):
-    
+
     def __init__(self, url):
         ComicSite.__init__(self, url)
+        self.imgurl = base64.urlsafe_b64decode( util.regexGroup(".+[?&]u=([^&]+)", self.url) ).decode("utf-8")
 
     def getPageNumber(self):
-        return util.regexGroup(".+/([0-9]+)\\.html$", self.url)
+        return util.regexGroup(".+/([0-9]+)\\.html", self.url)
 
     def getImageUrl(self):
-        doc = self.getDomObject()
-        img = doc.get_element_by_id("image")
-        return img.attrib['src']
+        return self.imgurl
